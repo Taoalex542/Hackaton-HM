72c2f8b13e9ef5f28e06d56fa0fc37d07cba0bc9
Flogrv <florian.grave@epitech.eu>
2025-01-14 10:42:41 +0100

    Push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyChart from './components/mychart' // Updated import to match your file name
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
+      {/* Add the RadarChart component here */}
+      <MyChart />
     </>
   )
 }
